73dbe71d9acf8aa4cf8ef364b0cb7510b323c04d
Pranaya Deomani <pranayadeomani@protonmail.com>
2021-11-11 12:58:10 +0530

    OplusKernelEnvConfig: Don't print allowed macros as warnings
    Change-Id: I7d1e7d6de216e0f094e444d0bc234879494242ca
    Signed-off-by: Pranaya Deomani <pranayadeomani@protonmail.com>

--- OplusKernelEnvConfig.mk
+++ OplusKernelEnvConfig.mk
@@ -103,7 +103,6 @@ OPLUS_FEATURE_SDCARD
 
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning myfeature is $(myfeature)) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
