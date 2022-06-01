2d92f25ea2aa2e9012413ab9c64a2100739c6b9d
Bharat Dev Burman <bharat.singh7924@gmail.com>
2022-06-01 22:26:16 +0530

    power: oplus: actually fix header inclusion
    This change will make it refer to the right relative location of the header file "tcpm.h"

--- drivers/power/oplus/charger_ic/oplus_battery_mtk6853R.h
+++ drivers/power/oplus/charger_ic/oplus_battery_mtk6853R.h
@@ -32,7 +32,7 @@
 #include <linux/uaccess.h>
 
 #include <mt-plat/charger_class.h>
-#include "tcpm.h"
+#include "../drivers/misc/mediatek/typec/tcpc/inc/tcpm.h"
 #include <mtk_direct_charge_vdm.h>
 struct charger_manager;
 #include "mtk_pe_intf.h"
