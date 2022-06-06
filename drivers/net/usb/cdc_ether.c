e03f977dd53dd86fe2e2fe3ab09a3b0045161c66
Anupam Roy <github.769@gmail.com>
2022-06-06 00:07:35 +0000

    Import drivers from realme8_5g source
    Signed-off-by: Anupam Roy <github.769@gmail.com>

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -736,6 +736,8 @@ static const struct usb_device_id	products[] = {
 },
 
 /* Realtek RTL8152 Based USB 2.0 Ethernet Adapters */
+#ifndef OPLUS_FEATURE_CHG_BASIC
+/*Boyu.Wen@BSP.CHG.Basic 2020/12/20 add for usb eth*/
 {
 	USB_DEVICE_AND_INTERFACE_INFO(REALTEK_VENDOR_ID, 0x8152, USB_CLASS_COMM,
 			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
@@ -748,6 +750,7 @@ static const struct usb_device_id	products[] = {
 			USB_CDC_SUBCLASS_ETHERNET, USB_CDC_PROTO_NONE),
 	.driver_info = 0,
 },
+#endif
 
 /* Samsung USB Ethernet Adapters */
 {
